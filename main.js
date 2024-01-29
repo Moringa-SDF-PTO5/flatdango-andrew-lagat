0c5e1dee507ad7237cc774fc2db967a02d42ccf6
maruands <maruands@gmail.com>
2024-01-29 09:42:33 +0300

    Delete section

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ cardDescription.className = "card-description"
 const films = document.querySelector('#films');
 let filmsData = []; 
 let available;
-
+let ticket_sold;
+let movieObj;
 // Selectors
 const content = document.querySelector("#content");
-// console.log(content)
 const cards = [];
 fetch(url)
 .then(data => data.json())
@@ -21,18 +21,21 @@ fetch(url)
     filmsData = res;
     //Initial diplay when the page loads
     renderMovies(res[0]);
-
+    // console.log(res[0])
     for(let i = 0; i < res.length; i++)
     {
         renderNav(res[i])
     }
-});
+})
+.catch(error => console.log(error));
 
 function renderMovies(movie)
 {
+    movieObj = movie;
     const movieDiv = document.createElement('div');
     movieDiv.innerHTML = '';
     available = (movie.capacity - movie.tickets_sold);
+    ticket_sold = movie.tickets_sold;
     movieDiv.innerHTML = `
         <div class="card">
             <img class="card-img" src="${movie.poster}">
@@ -42,7 +45,7 @@ function renderMovies(movie)
                 <p>Runtime: ${movie.runtime}</p>
                 <p>Availible tickets: <span id='available'>${available}</span></p>
                 <p>Show time: ${movie.showtime}</p>
-                <button class="buyTicket" onClick="buyTicket('${movie.id}')">Buy Ticket</button>
+                <button class="buyTicket" id="buyTicket" onClick="buyTicket('${movie.id}')">Buy Ticket</button>
             </div>
         </div>
     `
@@ -53,10 +56,11 @@ function renderMovies(movie)
 function renderNav(data)
 {
     films.className = "films"
-    const film = document.createElement('li')
+    let film = document.createElement('li')
     film.className = "film item"
     film.innerHTML = `
         <a onClick = "navigator('${data.id}')">${data.title}</a>
+        <a onClick = "deleteFilm('${data.id}')" style="color:red; float:right;">delete</a>
     `
     films.appendChild(film);
 }
@@ -67,16 +71,51 @@ function navigator(id)
 }
 
 function buyTicket(id){
-    console.log(filmsData[id-1])
     
-    // console.log(available)
     if(available > 0){
-        available = available - 1
+        available = available - 1;
+        ticket_sold = ticket_sold + 1;
+        updateServer(id,ticket_sold);
         document.getElementById('available').textContent = available;
-    } else {
-        const button = document.getElementsByClassName('buyTicket');
-        button.setAttribute("disabled","");
-    }
+        if (available === 0){
+            const button = document.getElementById("buyTicket");
+            button.textContent = "Ticket Sold Out";
+            button.style.backgroundColor = "red"
+            button.style.cursor = "not-allowed"
+            button.setAttribute("disabled", "");
+        }
+    } 
     
+}
+
+function updateServer(id,ticket_sold)
+{
+    // console.log(id,ticket_sold)
+    console.log(movieObj)
+    fetch(`${url}/${id}`,{
+        method: "PATCH",
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            "tickets_sold":ticket_sold
+        })
+        
+    })
+    .then(res => res.json())
+        .then(movie => {
+            renderMovies(movie);
+        })
+}
+
+function deleteFilm(id)
+{
+    console.log(id);
+    let msg = "Are you sure you want to delete";
+    if(confirm(msg) == true){
+        fetch(`${url}/${id}`,{
+            method: "DELETE",
+        })
+    }
 }
 
