1b13d6b3ef93996f11094453864d01fe8783b2a2
maruands <maruands@gmail.com>
2024-01-30 22:52:42 +0300

    done and dusted

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const url = "http://localhost:3000/films"
 
 //create elements
+const content = document.querySelector("#content");
 const card = document.createElement('div');
 card.className = "card"
 const cardImg = document.createElement('img')
@@ -9,19 +10,14 @@ const cardDescription = document.createElement('div');
 cardDescription.className = "card-description"
 const films = document.querySelector('#films');
 let filmsData = []; 
-let available;
-let ticket_sold;
-let movieObj;
-// Selectors
-const content = document.querySelector("#content");
-const cards = [];
+let available, ticket_sold, movieObj;
+
+
 fetch(url)
 .then(data => data.json())
 .then(res => {
     filmsData = res;
-    //Initial diplay when the page loads
     renderMovies(res[0]);
-    // console.log(res[0])
     for(let i = 0; i < res.length; i++)
     {
         renderNav(res[i])
@@ -36,6 +32,7 @@ function renderMovies(movie)
     movieDiv.innerHTML = '';
     available = (movie.capacity - movie.tickets_sold);
     ticket_sold = movie.tickets_sold;
+    
     movieDiv.innerHTML = `
         <div class="card">
             <img class="card-img" src="${movie.poster}">
@@ -49,9 +46,15 @@ function renderMovies(movie)
             </div>
         </div>
     `
+    
     document.querySelector("#movies").innerHTML = '';
     document.querySelector("#movies").appendChild(movieDiv);
-
+    if(movie.capacity == movie.tickets_sold ){
+        const button = document.getElementById("buyTicket")
+        button.className = "buyTicket button_sold_out";
+        button.textContent = "Ticket Sold Out";
+        button.setAttribute("disabled", "");
+    }
 }
 
 function renderNav(data)
@@ -59,6 +62,9 @@ function renderNav(data)
     films.className = "films"
     let film = document.createElement('li')
     film.className = "film item"
+    if(data.capacity == data.tickets_sold ){
+        film.className = "film item sold-out"
+    }
     film.innerHTML = `
         <a onClick = "navigator('${data.id}')">${data.title}</a>
         <a onClick = "deleteFilm('${data.id}')" style="color:red; float:right;">delete</a>
@@ -71,29 +77,21 @@ function navigator(id)
     renderMovies(filmsData[id-1]);
 }
 
-function buyTicket(id){
-    console.log((id));
+function buyTicket(e){
+    // e.preventDefault();
     if(available > 0){
         available = available - 1;
         ticket_sold = ticket_sold + 1;
-        updateServer(id,ticket_sold);
-        document.getElementById('available').textContent = available;
-        if (available === 0){
-            const button = document.getElementById("buyTicket");
-            button.textContent = "Ticket Sold Out";
-            button.style.backgroundColor = "red"
-            button.style.cursor = "not-allowed"
-            button.setAttribute("disabled", "");
-        }
+        document.getElementById('available').textContent = (available > 0) ? available : "0";
+        updateServer(ticket_sold);
     } 
     
 }
 
-function updateServer(id,ticket_sold)
+async function updateServer(ticket_sold)
 {
-    // console.log(id,ticket_sold)
-    // console.log(movieObj)
-    fetch(`${url}/${id}`,{
+    // console.log(ticket_sold)
+    await fetch(`${url}/${movieObj.id}`,{
         method: "PATCH",
         headers: {
             'Content-Type':'application/json'
@@ -102,16 +100,11 @@ function updateServer(id,ticket_sold)
             "tickets_sold":ticket_sold
         })
         
-    })
-    .then(res => res.json())
-    .then(movie => {
-        renderMovies(movie);
-    })
+    });
 }
 
 function deleteFilm(id)
 {
-    //console.log(id);
     let msg = "Are you sure you want to delete";
     if(confirm(msg) == true){
         fetch(`${url}/${id}`,{
